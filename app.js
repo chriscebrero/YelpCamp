8922c629a92a00bdf7f963666670e832b2221a50
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-07 21:00:42 +0000

    finished v2

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/90255/feature_Julia_Pfeiffer_Burns_State_Park-f2.jpg"},
+        {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/3880/feature_Nevada_St_Beach-f2.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://www.campsitephotos.com/photo/camp/33819/feature_Blue_Spring_State_Park-f4.jpg"}
+];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/90255/feature_Julia_Pfeiffer_Burns_State_Park-f2.jpg"},
-        {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/3880/feature_Nevada_St_Beach-f2.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://www.campsitephotos.com/photo/camp/33819/feature_Blue_Spring_State_Park-f4.jpg"}
-
-        ]
+    
     res.render("campgrounds", {campgrounds: campgrounds});
 })
 
 app.post("/campgrounds", function(req, res) {
-    res.send("You hit the post route!");
-    //get data from form and add to campgrounds array
-    // redirect back to campgrounds page
+    var name = req.body.name;
+    var image = req.body.image
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 })
 
 app.get("/campgrounds/new", function(req, res) {
