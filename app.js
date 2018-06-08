6d00d50b96de62a8d3761a61347da95e8f0b0fcd
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-08 04:00:29 +0000

    fixed desc

--- app.js
+++ app.js
@@ -52,7 +52,8 @@ app.post("/campgrounds", function(req, res) {
     // CREATE - adds new campgrounds
     var name = req.body.name;
     var image = req.body.image
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     //Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
