6249c37777ca09a751c7e6e64811366678901371
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-08 01:25:47 +0000

    put in mongoose

--- app.js
+++ app.js
@@ -3,23 +3,46 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://www.campsitephotos.com/photo/camp/90255/feature_Julia_Pfeiffer_Burns_State_Park-f2.jpg"},
-        {name: "Granite Hill", image: "https://www.campsitephotos.com/photo/camp/3880/feature_Nevada_St_Beach-f2.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://www.campsitephotos.com/photo/camp/33819/feature_Blue_Spring_State_Park-f4.jpg"}
-];
+// SCHEMA setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+Campground.create(
+    {
+        name: "Granite Hill",
+        image: "https://www.campsitephotos.com/photo/camp/3880/feature_Nevada_St_Beach-f2.jpg"
+        
+    },
+     function(err, campground){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("newly created campground: ");
+        console.log(campground);
+    }
+});
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    })
+    // res.render("campgrounds", {campgrounds: campgrounds});
 })
 
 app.post("/campgrounds", function(req, res) {
