8b5852c87b103e8698a6d621e8ab9ea79e0b50d1
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 05:52:12 +0000

    added comments

--- app.js
+++ app.js
@@ -65,7 +65,14 @@ app.get("/campgrounds/:id", function(req, res){
 //============================ COMMENTS
 
 app.get("/campgrounds/:id/comments/new", function(req, res) {
-    res.render("comments/new");
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    })
 })
 
 app.listen(process.env.PORT, process.env.IP, function() {
