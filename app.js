ade8c6948cee42191474b2e552dd6e76a9922089
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 21:42:03 +0000

    isloggedin implemented

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get("/campgrounds/:id", function(req, res){
 
 //============================ COMMENTS
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     // find campground by id
     Campground.findById(req.params.id, function(err, campground){
         if(err){
@@ -155,8 +155,14 @@ app.post("/login", passport.authenticate("local",
 app.get("/logout", function(req, res){
     req.logout();
     res.redirect("/campgrounds");
-})
+});
 
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YelpCamp has started!");
