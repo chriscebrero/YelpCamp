d7b4a44e4a4c97ced3420fb61c6e859c03a2c272
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 21:07:51 +0000

    added post register

--- app.js
+++ app.js
@@ -123,6 +123,20 @@ app.get("/register", function(req, res){
     res.render("register");
 })
 
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = ({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
 
 
 
