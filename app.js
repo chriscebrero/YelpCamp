3c71b165586a1d1ad3d4385e94e4dfc2560a5895
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-09 21:34:08 +0000

    added logout get

--- app.js
+++ app.js
@@ -142,11 +142,20 @@ app.get("/login", function(req, res){
     res.render("login");
 })
 // Handle LOGIN Logic
-app.post("/login", function(req, res){
-    res.send("logging in");
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+    
+});
+
+//logout route
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
 })
-
-
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
