fcfe2e5ba95c469543ab61c51a54d80fc4ae5dac
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-11 07:11:55 +0000

    flash on comments

--- middleware/index.js
+++ middleware/index.js
@@ -38,12 +38,14 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
                 if(foundComment.author.id.equals(req.user._id)) {
                 next();
             } else {
+                req.flash("error", "You don't have permission to do that!");
                 res.redirect("back");
             }
         }
     });
         
     } else {
+        req.flash("error", "You need to be logged in to do that!");
         res.redirect("back");
     }
 }
